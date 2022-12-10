e42ccd233ae6f5f3ee15cab027bb5d132f15e0a1
Elhadji Oumar Diouf <elho.diouf1@uvs.edu.sn>
2022-12-10 02:57:06 +0000

    update task 5 printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,21 @@ int main(void)
 {
 	int a;
 	long int b;
+	long long c;
 	char d;
 	float e;
 
-	printf("Size of an int: %d byte(s)\n", (int) sizeof(a));
-	printf("Size of a long int: %li byte(s)\n", (int long) sizeof(b));
-	printf("Size of an char: %c byte(s)\n", (char) sizeof(d));
-	printf("Size of an float: %f byte(s)\n", (float) sizeof(e));
+	/**printf("Size of an int: %d byte(s)\n", (int) sizeof(a));
+	*printf("Size of a long long int: %lli byte(s)\n", (long long int) sizeof(c));
+	*printf("Size of a long int: %li byte(s)\n", (int long) sizeof(b));
+	*printf("Size of an char: %c byte(s)\n", (char) sizeof(d));
+	*printf("Size of an float: %f byte(s)\n", (float) sizeof(e));
+	*return (0);
+	*/
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
